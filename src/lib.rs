f219c375386a2cc9d1a5fae4a7e799dc649fa39b
Brad Alfirevic <brad@genbyte.dev>
2021-04-09 19:55:00 -0500

    Quick hack to cut image to actual size

--- src/lib.rs
+++ src/lib.rs
@@ -16,14 +16,43 @@ pub fn read_file(filename: &str) -> Image<Sensor, u16> {
 	let sensor_data = (*decoded).to_vec();
 	let sizes = decoded.sizes();
 
-	Image {
-		kind: Sensor {},
-		data: sensor_data,
-		meta: Metadata::new(
-			sizes.raw_width as u32, 
-			sizes.raw_height as u32,
-			CFA::RGGB,
-			decoded.color()
-		)
+	let raw_size = sizes.raw_width as usize * sizes.raw_height as usize;
+	let image_size = sizes.width as usize * sizes.height as usize;
+
+	// TODO: Move to own function, call `extract_meaningful_image` maybe?
+	if raw_size != image_size {
+		let mut image = Vec::with_capacity(image_size);
+
+		// FIXME: Assumes the extra data is to the right and/or bottom
+		for row in 0..sizes.height as usize {
+			let lower = row * sizes.raw_width as usize;
+			let upper = lower + sizes.width as usize;
+
+			image.extend_from_slice(&sensor_data[lower..upper]);
+		}
+
+		//FIXME: Assumes CFA:RGGB
+		Image {
+			kind: Sensor {},
+			data: image,
+			meta: Metadata::new(
+				sizes.width as u32, 
+				sizes.height as u32,
+				CFA::RGGB,
+				decoded.color()
+			)
+		}
+	} else {
+		//FIXME: Assumes CFA:RGGB
+		Image {
+			kind: Sensor {},
+			data: sensor_data,
+			meta: Metadata::new(
+				sizes.raw_width as u32, 
+				sizes.raw_height as u32,
+				CFA::RGGB,
+				decoded.color()
+			)
+		}
 	}
 }
